9e58b829471ebe50312aaa2bbabbf088c550ee40
Vetle Mangrud Refsnes <vmangrud@gmail.com>
2025-08-20 13:14:25 +0200

    add back on_attach for lsp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -323,15 +323,15 @@ mason_lspconfig.setup {
   automatic_installation = false,
   handlers = {
     function(server_name)
-      local server = servers[server_name] or {}
-      -- This handles overriding only values explicitly passed
-      -- by the server configuration above. Useful when disabling
-      -- certain features of an LSP (for example, turning off formatting for ts_ls)
-      server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-      require('lspconfig')[server_name].setup(server)
+    require('lspconfig')[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+    }
     end,
   },
 }
+
 
 
 -- Turn on format on Save
